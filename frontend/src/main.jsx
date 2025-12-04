cb81944212b3f1b6bff2bb5d59627598a7b68fad
waqued-code <waqued@icloud.com>
2025-12-04 14:00:23 -0400

    Fix: Add basename to BrowserRouter for /app subdirectory

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,7 +11,7 @@ import theme from './theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/app">
       <ThemeProvider theme={theme}>
         <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="pt-br">
           <CssBaseline />
